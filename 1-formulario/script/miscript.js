a28f1679e52d12326f8c205ab2f6f72de627e965
Dylan Cifuentes Suaza <you@example.com>
2026-09-24 17:27:18 +0200

    feat: Añadiendo JS

---
+++ 1-formulario/script/miscript.js
@@ -0,0 +1,18 @@
+console.log("Prueba hecha.");
+console.warn("Esto es un warning");
+console.error("Esto es un error");
+
+// var hola = 'Saludo';
+let quetal = 'Saludo 2';
+const comoEstas = 'Bien';
+
+
+console.log(document.getElementById("titulo"));
+console.log(document.getElementById("nombre"));
+console.log(document.getElementById("apellidos"));
+console.log(document.getElementById("sexo"));
+console.log(document.getElementById("nick"));
+console.log(document.getElementById("email"));
+console.log(document.getElementById("comentario"));
+console.log(document.getElementById("enviar"));
+console.log(document.getElementById("reject"));
